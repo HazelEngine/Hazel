e71411659f4d44ed0022594381f4765398996818
Felipe Maciel <fvieiramacieldesouza58@gmail.com>
2018-11-11 22:28:15 -0200

    Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@
 
 	int main()
 	{
+		Hazel::Log::Init();
+		HZ_CORE_WARN("Initialized log system!")
+		HZ_INFO("Hello World! Value = {0}", 5)
+
 		auto app = Hazel::CreateApplication();
 		app->Run();
 		delete app;
