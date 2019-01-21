b56b1f9029c5e0c19e9149d3afc495fd1dde010f
Felipe Maciel <mrchampz@outlook.com>
2019-01-21 10:13:18 -0200

    ImGui Events

--- Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -3,9 +3,18 @@
 
 #include "Hazel/Application.h"
 #include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+#include "Platform/Windows/WindowsWindow.h"
+
+// TEMPORARY
 #include "GLFW/glfw3.h"
+#include "glad/glad.h"
 
 namespace Hazel {
+
+	GLFWcursor* g_MouseCursors[ImGuiMouseCursor_COUNT] = { 0 };
+
+	void UpdateMousePosAndButtons();
+	void UpdateMouseCursor();
 
 	ImGuiLayer::ImGuiLayer() :
 		Layer("ImGuiLayer")
@@ -26,27 +35,36 @@ namespace Hazel {
 		io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
 
 		// TEMPORARY: should eventually use Hazel key codes
-		io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-		io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-		io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-		io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-		io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-		io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-		io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-		io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-		io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-		io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
-		io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-		io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-		io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
-		io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-		io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-		io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-		io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-		io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-		io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-		io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-		io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+		io.KeyMap[ImGuiKey_Tab]                         = GLFW_KEY_TAB;
+		io.KeyMap[ImGuiKey_LeftArrow]                   = GLFW_KEY_LEFT;
+		io.KeyMap[ImGuiKey_RightArrow]                  = GLFW_KEY_RIGHT;
+		io.KeyMap[ImGuiKey_UpArrow]                     = GLFW_KEY_UP;
+		io.KeyMap[ImGuiKey_DownArrow]                   = GLFW_KEY_DOWN;
+		io.KeyMap[ImGuiKey_PageUp]                      = GLFW_KEY_PAGE_UP;
+		io.KeyMap[ImGuiKey_PageDown]                    = GLFW_KEY_PAGE_DOWN;
+		io.KeyMap[ImGuiKey_Home]                        = GLFW_KEY_HOME;
+		io.KeyMap[ImGuiKey_End]                         = GLFW_KEY_END;
+		io.KeyMap[ImGuiKey_Insert]                      = GLFW_KEY_INSERT;
+		io.KeyMap[ImGuiKey_Delete]                      = GLFW_KEY_DELETE;
+		io.KeyMap[ImGuiKey_Backspace]                   = GLFW_KEY_BACKSPACE;
+		io.KeyMap[ImGuiKey_Space]                       = GLFW_KEY_SPACE;
+		io.KeyMap[ImGuiKey_Enter]                       = GLFW_KEY_ENTER;
+		io.KeyMap[ImGuiKey_Escape]                      = GLFW_KEY_ESCAPE;
+		io.KeyMap[ImGuiKey_A]                           = GLFW_KEY_A;
+		io.KeyMap[ImGuiKey_C]                           = GLFW_KEY_C;
+		io.KeyMap[ImGuiKey_V]                           = GLFW_KEY_V;
+		io.KeyMap[ImGuiKey_X]                           = GLFW_KEY_X;
+		io.KeyMap[ImGuiKey_Y]                           = GLFW_KEY_Y;
+		io.KeyMap[ImGuiKey_Z]                           = GLFW_KEY_Z;
+
+		g_MouseCursors[ImGuiMouseCursor_Arrow]          = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_TextInput]      = glfwCreateStandardCursor(GLFW_IBEAM_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_ResizeAll]      = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_ResizeNS]       = glfwCreateStandardCursor(GLFW_VRESIZE_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_ResizeEW]       = glfwCreateStandardCursor(GLFW_HRESIZE_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_ResizeNESW]     = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_ResizeNWSE]     = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		g_MouseCursors[ImGuiMouseCursor_Hand]           = glfwCreateStandardCursor(GLFW_HAND_CURSOR);
 
 		ImGui_ImplOpenGL3_Init("#version 410");
 	}
@@ -78,6 +96,80 @@ namespace Hazel {
 
 	void ImGuiLayer::OnEvent(Event& event)
 	{
+		EventDispatcher dispatcher(event);
+		dispatcher.Dispatch<MouseButtonPressedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
+		dispatcher.Dispatch<MouseButtonReleasedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
+		dispatcher.Dispatch<MouseMovedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseMovedEvent));
+		dispatcher.Dispatch<MouseScrolledEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseScrolledEvent));
+		dispatcher.Dispatch<KeyPressedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
+		dispatcher.Dispatch<KeyReleasedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
+		dispatcher.Dispatch<KeyTypedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
+		dispatcher.Dispatch<WindowResizeEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnWindowResizedEvent));
+	}
+
+	bool ImGuiLayer::OnMouseButtonPressedEvent(MouseButtonPressedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.MouseDown[event.GetMouseButton()] = true;
+		return false;
+	}
+
+	bool ImGuiLayer::OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.MouseDown[event.GetMouseButton()] = false;
+		return false;
+	}
+
+	bool ImGuiLayer::OnMouseMovedEvent(MouseMovedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.MousePos = ImVec2(event.GetX(), event.GetY());
+		return false;
+	}
+
+	bool ImGuiLayer::OnMouseScrolledEvent(MouseScrolledEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.MouseWheelH += event.GetXOffset();
+		io.MouseWheel += event.GetYOffset();
+		return false;
+	}
+
+	bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.KeysDown[event.GetKeyCode()] = true;
+		io.KeyCtrl = io.KeysDown[GLFW_KEY_LEFT_CONTROL] || io.KeysDown[GLFW_KEY_RIGHT_CONTROL];
+		io.KeyShift = io.KeysDown[GLFW_KEY_LEFT_SHIFT] || io.KeysDown[GLFW_KEY_RIGHT_SHIFT];
+		io.KeyAlt = io.KeysDown[GLFW_KEY_LEFT_ALT] || io.KeysDown[GLFW_KEY_RIGHT_ALT];
+		io.KeySuper = io.KeysDown[GLFW_KEY_LEFT_SUPER] || io.KeysDown[GLFW_KEY_RIGHT_SUPER];
+		return false;
+	}
+
+	bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.KeysDown[event.GetKeyCode()] = false;
+		return false;
+	}
+
+	bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		int keycode = event.GetKeyCode();
+		if (keycode > 0 && keycode < 0x10000)
+			io.AddInputCharacter((unsigned short)keycode);
+		return false;
+	}
+
+	bool ImGuiLayer::OnWindowResizedEvent(WindowResizeEvent& event)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.DisplaySize = ImVec2(event.GetWidth(), event.GetHeight());
+		io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
+		glViewport(0.0f, 0.0f, event.GetWidth(), event.GetHeight());
+		return false;
 	}
 
 }
--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -22,6 +22,9 @@ namespace Hazel {
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		// TEMPORARY: may be removed
+		inline GLFWwindow* Get() { return m_Window; }
+
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
