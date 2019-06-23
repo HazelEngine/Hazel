a55b2e77ae5a5976a5663d3e64fe818895be7c4b
Felipe <mrchampz@outlook.com>
2019-06-23 15:14:56 -0300

    Renderer Flow and Submission

--- Hazel/src/Hazel/Renderer/Buffer.cpp
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -11,11 +11,11 @@ namespace Hazel {
 	{
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::None:
+			case RendererAPI::API::None:
 				HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 				return nullptr;
 
-			case RendererAPI::OpenGL:
+			case RendererAPI::API::OpenGL:
 				return new OpenGLVertexBuffer(vertices, count);
 
 			default:
@@ -28,11 +28,11 @@ namespace Hazel {
 	{
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::None:
+			case RendererAPI::API::None:
 				HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 				return nullptr;
 
-			case RendererAPI::OpenGL:
+			case RendererAPI::API::OpenGL:
 				return new OpenGLIndexBuffer(indices, count);
 
 			default:
